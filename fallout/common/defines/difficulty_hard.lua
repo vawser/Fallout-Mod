6b4261b2235fea7afaeba0e564c3f0f575a78191
Vawser <ben.rouncefield@gmail.com>
2020-04-20 23:01:12 +0100

    Restructure
    Added mod files

---
+++ fallout/common/defines/difficulty_hard.lua
@@ -0,0 +1,8 @@
+-- Define overrides when playing on Hard
+
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+NDefines.NAI.DIPLOMATIC_ACTION_OFFER_CONDOTTIERI_ONLY_MILITARY_RULERS = 0
+NDefines.NAI.DIPLOMATIC_ACTION_OFFER_CONDOTTIERI_ONLY_NEIGHBORS = 0
