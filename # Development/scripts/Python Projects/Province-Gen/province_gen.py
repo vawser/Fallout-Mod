d867c2c55f9d8ae87c26eb2d9442e9960c05fe54
Vawser <ben.rouncefield@gmail.com>
2023-05-15 16:30:12 +0100

    Commit
    - Enclave missions WIP

--- # Development/scripts/Python Projects/Province-Gen/province_gen.py
+++ # Development/scripts/Python Projects/Province-Gen/province_gen.py
@@ -1,10 +1,16 @@
 import os
+import os
+import xylozi_common as com
+import random
 
 start_id = 1
-end_id = 5000
+end_id = 1037
 
-os.chdir("C:\\Users\\Xylozi\\Documents\\Paradox Interactive\\Europa Universalis IV\\mod\\debug_mod\\history\\provinces\\")
+template = com.get_file_as_string("template.txt")
+
+os.chdir("C:\\Users\\Xylozi\\Documents\\Paradox Interactive\\Europa Universalis IV\\mod\\crimson_luna\\history\\provinces\\")
 
 for x in range(start_id, end_id):
     outputFile = open("{0} - Province.txt".format(x), "wt")
+    outputFile.write(template.format(random.randrange(1,10), random.randrange(1,10), random.randrange(1,10)))
     outputFile.close()
--- # Development/scripts/Python Projects/quick.py
+++ # Development/scripts/Python Projects/quick.py
@@ -1,5 +1,6 @@
 line = ""
-for x in range(1, 5000):
+for x in range(1, 1037):
     line = line + " " + str(x)
+    print(" PROV{0}: \"PROV{0}\"".format(x))
 
-print(line)
+#print(line)
