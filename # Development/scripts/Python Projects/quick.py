e0f9e5abbc2840e20f0b7101af79d809b9194a8b
Vawser <ben.rouncefield@gmail.com>
2023-05-15 19:32:42 +0100

    Commit

--- # Development/scripts/Python Projects/quick.py
+++ # Development/scripts/Python Projects/quick.py
@@ -1,6 +1,6 @@
 line = ""
-for x in range(1, 1037):
+for x in range(1038, 2000):
     line = line + " " + str(x)
-    print(" PROV{0}: \"PROV{0}\"".format(x))
+    #print(" PROV{0}: \"PROV{0}\"".format(x))
 
-#print(line)
+print(line)
